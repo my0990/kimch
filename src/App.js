5dab0cc7aedce97cd50d9beb732a5d85c195bcd4
my0990 <my0990@naver.com>
2022-07-02 23:08:40 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import kimch from './media2.gif';
+import kimchAudio from './kimchAudio.mp3';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>kimch</h1>
+        <img src={kimch}></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
