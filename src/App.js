4ecdf4917340ded177422b134189591f360b5858
my0990 <my0990@naver.com>
2022-07-10 20:00:54 +0900

    temp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import kimch from './media2.gif';
 import kimchAudio from './kimchAudio.mp3';
+import {useState} from 'react';
 
 function App() {
+  const [isClicked,setIsClicked] = useState({kimch});
   return (
     <div className="App">
       <header className="App-header">
         <h1>kimch</h1>
-        <img src={kimch}></img>
+        <button onClick={()=>{setIsClicked( prev => !prev)}}>버튼</button>
+        <img src={isClicked ? kimch : null}></img>
       </header>
     </div>
   );
